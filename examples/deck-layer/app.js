a77a8749e5204768e8703bbc5e6a49dc831ded82
Chris Gervang <chris@gervang.com>
2020-07-12 21:22:21 -0700

    WIP example

---
+++ examples/deck-layer/app.js
@@ -0,0 +1,58 @@
+import React, {useState, useRef} from 'react';
+import DeckGL from '@deck.gl/react';
+import {DeckAdapter} from '@hubble.gl/core';
+import {useNextFrame, BasicControls} from '@hubble.gl/react';
+import {sceneBuilder} from './scene';
+
+const INITIAL_VIEW_STATE = {
+  longitude: -122.4,
+  latitude: 37.74,
+  zoom: 11,
+  pitch: 30,
+  bearing: 0
+};
+
+const adapter = new DeckAdapter(sceneBuilder);
+
+/** @type {import('@hubble.gl/core/src/types').FrameEncoderSettings} */
+const encoderSettings = {
+  framerate: 30,
+  webm: {
+    quality: 0.8
+  },
+  jpeg: {
+    quality: 0.8
+  },
+  gif: {
+    sampleInterval: 1000
+  }
+};
+
+export default function App() {
+  const deckgl = useRef(null);
+  const [ready, setReady] = useState(false);
+  const [busy, setBusy] = useState(false);
+  const nextFrame = useNextFrame();
+
+  return (
+    <div style={{position: 'relative'}}>
+      <DeckGL
+        width={720}
+        height={480}
+        ref={deckgl}
+        initialViewState={INITIAL_VIEW_STATE}
+        {...adapter.getProps(deckgl, setReady, nextFrame)}
+      />
+      <div style={{position: 'absolute'}}>
+        {ready && (
+          <BasicControls
+            adapter={adapter}
+            busy={busy}
+            setBusy={setBusy}
+            encoderSettings={encoderSettings}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
