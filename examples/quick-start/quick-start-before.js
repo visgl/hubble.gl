43e90344499911b5fbf0ec4030b0262b052109c2
Chris Gervang <chris@gervang.com>
2021-01-01 21:27:33 -0800

    before and after

---
+++ examples/quick-start/quick-start-before.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {ScatterplotLayer, TextLayer} from '@deck.gl/layers';
+import DeckGL from '@deck.gl/react';
+
+const INITIAL_VIEW_STATE = {
+  longitude: -122.402,
+  latitude: 37.79,
+  zoom: 11
+};
+
+export default function App() {
+  const layers = [
+    new ScatterplotLayer({
+      data: [{position: [-122.402, 37.79], color: [255, 0, 0], radius: 1000}],
+      getFillColor: d => d.color,
+      getRadius: d => d.radius
+    }),
+    new TextLayer({
+      data: [{position: [-122.402, 37.79], text: 'Hello World'}]
+    })
+  ];
+
+  return (
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      width={640}
+      height={480}
+      layers={layers}
+      parameters={{
+        clearColor: [255, 255, 255, 1]
+      }}
+    />
+  );
+}
