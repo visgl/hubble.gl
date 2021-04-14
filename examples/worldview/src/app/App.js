7533ad5962647f14a8035ec348c1aee99730d977
chrisgervang <chris@gervang.com>
2021-04-14 14:09:27 -0700

    Switch App to a functional component

--- examples/worldview/src/app/App.js
+++ examples/worldview/src/app/App.js
@@ -18,10 +18,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import styled, {ThemeProvider} from 'styled-components';
-import window from 'global/window';
-import {connect} from 'react-redux';
 import {IntlProvider} from 'react-intl';
 import {messages} from 'kepler.gl/localization';
 import {theme} from 'kepler.gl/styles';
@@ -29,6 +27,7 @@ import {registerEntry} from 'kepler.gl/actions';
 import {AUTH_TOKENS} from '../constants';
 
 import {Stage} from '../features/stage/Stage';
+import {useDispatch, useSelector} from 'react-redux';
 
 import {InjectKeplerUI} from '@hubble.gl/react';
 import {LoadingSpinner} from 'kepler.gl/components';
@@ -37,6 +36,7 @@ const KEPLER_UI = {
 };
 
 import {loadSampleData} from '../data/sample';
+// import {loadJson} from '../data/json';
 
 // const keplerGlGetState = state => state.demo.keplerGl;
 
@@ -80,15 +80,11 @@ const WindowSize = styled.div`
   background-color: #0e0e10;
 `;
 
-class App extends Component {
-  state = {
-    width: window.innerWidth,
-    height: window.innerHeight
-  };
-
-  componentDidMount() {
-    // add a new entry to reducer
-    this.props.dispatch(
+const App = ({}) => {
+  const dispatch = useDispatch();
+  const hubbleGl = useSelector(state => state.hubbleGl);
+  useEffect(() => {
+    dispatch(
       registerEntry({
         id: 'map',
         mint: true,
@@ -98,45 +94,40 @@ class App extends Component {
         // initialUiState
       })
     );
+  }, []);
 
+  useEffect(() => {
     // load sample data
     const actions = loadSampleData();
-    actions.forEach(a => this.props.dispatch(a));
-  }
+    actions.forEach(a => dispatch(a));
 
-  render() {
-    return (
-      <IntlProvider locale="en" messages={messages.en}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle
-            // this is to apply the same modal style as kepler.gl core
-            // because styled-components doesn't always return a node
-            // https://github.com/styled-components/styled-components/issues/617
-            ref={node => {
-              if (node) {
-                this.root = node;
-              }
-            }}
-          >
-            <WindowSize>
-              <InjectKeplerUI keplerUI={KEPLER_UI}>
-                {this.props.hubbleGl.map.ready && (
-                  <>
-                    <div style={{height: 400, margin: 16}}>
-                      <Stage />
-                    </div>
-                  </>
-                )}
-              </InjectKeplerUI>
-            </WindowSize>
-          </GlobalStyle>
-        </ThemeProvider>
-      </IntlProvider>
-    );
-  }
-}
+    // loadJson().then(data => {
+    //   // console.log(data);
+    //   dispatch(data);
+    //   // console.log(this.props);
+    //   // dispatch(mapStyleChange({styleType: "light"}))
+    // });
+  }, []);
 
-const mapStateToProps = state => state;
-const dispatchToProps = dispatch => ({dispatch});
+  return (
+    <IntlProvider locale="en" messages={messages.en}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle>
+          <WindowSize>
+            <InjectKeplerUI keplerUI={KEPLER_UI}>
+              {hubbleGl.map.ready && (
+                <>
+                  <div style={{height: 1080, margin: 16}}>
+                    <Stage />
+                  </div>
+                </>
+              )}
+            </InjectKeplerUI>
+          </WindowSize>
+        </GlobalStyle>
+      </ThemeProvider>
+    </IntlProvider>
+  );
+};
 
-export default connect(mapStateToProps, dispatchToProps)(App);
+export default App;
