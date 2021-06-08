55585b30b461c9a007df2de1068ed1131aa0f8fa
chrisgervang <chris@gervang.com>
2021-06-08 12:33:35 -0700

    Add decimalPlaces param

--- examples/worldview/src/features/stage/utils.js
+++ examples/worldview/src/features/stage/utils.js
@@ -1,7 +1,8 @@
 export const nearestEvenInt = num => 2 * Math.round(num / 2);
 
-export const nearestEvenFloat = num => {
-  num = num * 1000;
+export const nearestEvenFloat = (num, decimalPlaces = 3) => {
+  const factorTen = Math.pow(10, decimalPlaces);
+  num = num * factorTen;
   num = 2 * Math.round(num / 2);
-  return num / 1000;
+  return num / factorTen;
 };
