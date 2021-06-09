adfea66904ee6a89ada8e9364f4046f31ea35df9
chrisgervang <chris@gervang.com>
2021-06-08 22:18:01 -0700

    [feat] Add tripLayerKeyframe for Kepler Trip Layer

--- examples/worldview/src/features/timeline/timelineSlice.js
+++ examples/worldview/src/features/timeline/timelineSlice.js
@@ -4,6 +4,7 @@ import {createSlice} from '@reduxjs/toolkit';
 const initialState = {
   cameraKeyframes: undefined, // keyframe object
   filterKeyframes: undefined, // keyframe object
+  tripLayerKeyframes: undefined, // keyframe object
   layerKeyframes: {}, // name: keyframe object
   frame: {}
 };
@@ -14,6 +15,7 @@ const timelineSlice = createSlice({
   reducers: {
     updateCameraKeyframes: (state, action) => void (state.cameraKeyframes = action.payload),
     updateFilterKeyframes: (state, action) => void (state.filterKeyframes = action.payload),
+    updateTripLayerKeyframes: (state, action) => void (state.tripLayerKeyframes = action.payload),
     updateLayerKeyframes: (state, action) =>
       void (state.layerKeyframes = {...state.layerKeyframes, ...action.payload}),
     updateFrame: (state, action) => void (state.frame = action.payload)
@@ -24,6 +26,7 @@ export const {
   updateCameraKeyframes,
   updateFilterKeyframes,
   updateLayerKeyframes,
+  updateTripLayerKeyframes,
   updateFrame
 } = timelineSlice.actions;
 
@@ -35,5 +38,6 @@ export default timelineSlice.reducer;
 
 export const cameraKeyframeSelector = state => state.hubbleGl.timeline.cameraKeyframes;
 export const filterKeyframeSelector = state => state.hubbleGl.timeline.filterKeyframes;
+export const tripLayerKeyframeSelector = state => state.hubbleGl.timeline.tripLayerKeyframes;
 export const layerKeyframeSelector = state => state.hubbleGl.timeline.layerKeyframes;
 export const frameSelector = state => state.hubbleGl.timeline.frame;
