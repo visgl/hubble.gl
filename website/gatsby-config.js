a3352fe1a83f0ad3db1ace29fcce894704f2df3b
Chris Gervang <chrisgervang@users.noreply.github.com>
2022-08-05 18:10:13 -0700

    fix(website) builder kepler separately and unpin deck version (#255)

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,4 +1,6 @@
-const resolve = require('path').resolve;
+const {resolve} = require('path');
+
+const ROOT_DIR = resolve('..');
 
 module.exports = {
   plugins: [
@@ -9,45 +11,42 @@ module.exports = {
 
         // Folders
         DIR_NAME: __dirname,
-        ROOT_FOLDER: `${__dirname}/../`,
+        ROOT_FOLDER: ROOT_DIR,
 
         DOCS: require('../docs/table-of-contents.json'),
         DOC_FOLDERS: [
-          `${__dirname}/../docs/`
+          resolve(ROOT_DIR, 'docs'),
         ],
         SOURCE: [
-          `${__dirname}/static`,
-          `${__dirname}/src`
+          resolve('./static'),
+          resolve('./src')
         ],
 
         PROJECT_TYPE: 'github',
 
         PROJECT_NAME: 'hubble.gl',
-        PROJECT_ORG: 'uber',
+        PROJECT_ORG: 'visgl',
         PROJECT_ORG_LOGO: 'images/visgl-logo.png',
         PROJECT_URL: 'https://hubble.gl',
         PROJECT_DESC: 'High quality client-side 3d animation and video rendering',
-        PATH_PREFIX: '/',
+        
+        // This is only activated in staging, with `gatsby build --prefix-paths`
+        PATH_PREFIX: '/hubble.gl',
+        
         THEME_OVERRIDES: '',
-
-        GA_TRACKING: null,
-
-        // For showing star counts and contributors.
-        // Should be like btoa('YourUsername:YourKey') and should be readonly.
-        GITHUB_KEY: null,
 
         HOME_PATH: '',
         LINK_TO_GET_STARTED: '/docs',
         PAGES: [
           {
             path: '/',
-            componentUrl: resolve(__dirname, './templates/index.jsx'),
+            componentUrl: resolve(__dirname, './src/index.js'),
             content: ''
           },
           {
             title: 'Showcase',
             path: '/showcase',
-            componentUrl: resolve(__dirname, './src/pages/showcase.jsx'),
+            componentUrl: resolve('./src/showcase.js'),
           }
         ],
 
@@ -64,44 +63,13 @@ module.exports = {
         ],
 
         STYLESHEETS: [''],
-        EXAMPLES: [
-          {
-            title: 'Getting Started: Basemap',
-            path: 'examples/basic-basemap/',
-            image: 'images/demo-thumb-basic-basemap.png',
-            componentUrl: resolve('../examples/basic-basemap/app.js')
-          },
-          {
-            title: 'Getting Started: Hello World',
-            path: 'examples/hello-world/',
-            image: 'images/demo-thumb-hello-world.png',
-            componentUrl: resolve('../examples/quick-start/app.js')
-          },
-          {
-            title: 'Animate Camera',
-            path: 'examples/camera/',
-            image: 'images/demo-thumb-animate-camera.png',
-            componentUrl: resolve('../examples/camera/app.js')
-          },
-          {
-            title: 'Landmark Tour',
-            path: 'examples/terrain/',
-            image: 'images/demo-thumb-terrain.jpg',
-            componentUrl: resolve('../examples/terrain/app.js')
-          },
-          {
-            title: 'Kepler.gl Animation',
-            path: 'examples/kepler-integration/',
-            image: 'images/demo-thumb-kepler-example.jpg',
-            componentUrl: resolve('./src/pages/kepler-example.jsx')
-          },
-          {
-            title: 'NYC Trips',
-            path: 'examples/nyc-trips/',
-            image: 'images/demo-thumb-nyc-trips.png',
-            componentUrl: resolve('../examples/trips/app.js')
-          }
-        ],
+        EXAMPLES: require('./examples.js'),
+
+        GA_TRACKING_ID: null,
+
+        // For showing star counts and contributors.
+        // Should be like btoa('YourUsername:YourKey') and should be readonly.
+        GITHUB_KEY: null
       }
     },
     {resolve: 'gatsby-plugin-no-sourcemaps'}
--- website/src/jsRoutes.js
+++
@@ -1,9 +0,0 @@
-import Showcase from './pages/showcase'
-
-export default [
-    {
-        name: "Showcase",
-        path: "/showcase",
-        component: Showcase
-    }
-]
