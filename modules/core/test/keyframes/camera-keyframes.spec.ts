7a1df8463183c1abefa14acaa7bc6c2195c194a9
Chris Gervang <chris@gervang.com>
2024-05-02 15:09:13 -0700

    chore: fix test runner (but not all tests)
    Signed-off-by: Chris Gervang <chris@gervang.com>

--- modules/core/test/keyframes/camera-keyframes.spec.ts
+++ modules/core/test/keyframes/camera-keyframes.spec.ts
@@ -4,8 +4,34 @@
 import test from 'tape-catch';
 import {CameraKeyframes, hold} from '@hubble.gl/core';
 import {easeInOut} from 'popmotion';
-import {toLowPrecision} from '@deck.gl/test-utils';
 import {flyToInterpolator} from '@hubble.gl/core/keyframes/camera-keyframes';
+
+function toLowPrecision(input: number, precision?: number): number;
+function toLowPrecision(input: number[], precision?: number): number[];
+function toLowPrecision(input: Record<string, number>, precision?: number): Record<string, number>;
+
+/**
+ * Covert all numbers in a deep structure to a given precision, allowing
+ * reliable float comparisons. Converts data in-place.
+ */
+function toLowPrecision(
+  input: number | number[] | Record<string, number>,
+  precision: number = 11
+): number | number[] | Record<string, number> {
+  /* eslint-disable guard-for-in */
+  if (typeof input === 'number') {
+    return Number(input.toPrecision(precision));
+  }
+  if (Array.isArray(input)) {
+    return input.map(item => toLowPrecision(item, precision));
+  }
+  if (typeof input === 'object') {
+    for (const key in input) {
+      input[key] = toLowPrecision(input[key], precision);
+    }
+  }
+  return input;
+}
 
 /* eslint-disable max-len */
 const TEST_CASES = [
--- modules/react/src/components/export-video/utils.ts
+++ modules/react/src/components/export-video/utils.ts
@@ -3,7 +3,8 @@
 // Copyright (c) vis.gl contributors
 import {point} from '@turf/helpers';
 import transformTranslate from '@turf/transform-translate';
-import {WebMercatorViewport, MapViewState} from '@deck.gl/core/typed';
+import {WebMercatorViewport} from '@deck.gl/core';
+import type {MapViewState} from '@deck.gl/core/typed';
 
 export function scaleToVideoExport(
   viewState: MapViewState,
