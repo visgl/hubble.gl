d5911706c71ddd98500229ad8e10d549caee1f85
Chris Gervang <chrisgervang@users.noreply.github.com>
2025-02-04 17:02:54 -0800

    chore(main) add top-level import test (#298)
    * chore(main) add top-level import test
    
    * fix(tests) disable node tests

--- test/modules.ts
+++ test/modules.ts
@@ -1,6 +1,7 @@
 // hubble.gl
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
+import './imports-spec';
 import '../modules/core/test/index';
 import '../modules/main/test/index';
 import '../modules/react/test/index';
