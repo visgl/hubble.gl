ca5e3ea5015d829fcf0e0ced6a123916730998e5
Chris Gervang <chris@gervang.com>
2025-02-04 16:44:52 -0800

    fix(react) kepler layers should be visible

--- modules/react/src/kepler-layers.ts
+++ modules/react/src/kepler-layers.ts
@@ -4,34 +4,59 @@
 
 import {createSelector} from 'reselect';
 import type {Layer, MapViewState} from '@deck.gl/core/typed';
-
+import type {KeplerGlState} from '@kepler.gl/reducers';
+import type {Layer as KeplerLayer} from '@kepler.gl/layers';
+import type {SplitMap, SplitMapLayers} from '@kepler.gl/types';
 /**
  * Kepler Layer Creation
  * Forked from kepler.gl
  * https://github.com/keplergl/kepler.gl/blob/master/src/components/map-container.js
  */
-const layersSelector = (state: any) => state.visState.layers;
-const layerDataSelector = (state: any) => state.visState.layerData;
-const mapLayersSelector = (state: any) => state.visState.mapLayers;
+const layersSelector = (state: KeplerGlState) => state.visState.layers;
+const layerDataSelector = (state: KeplerGlState) => state.visState.layerData;
+// const mapLayersSelector = (state: KeplerGlState) => state.visState.mapLayers;
+
+const getMapLayersFromSplitMaps = (
+  splitMaps: SplitMap[],
+  mapIndex?: number
+): SplitMapLayers | undefined | null => {
+  return splitMaps[mapIndex || 0]?.layers;
+};
+
+const splitMapSelector = (state: KeplerGlState) => state.visState.splitMaps;
+const splitMapIndexSelector = (_: KeplerGlState, mapIndex: number | undefined) => mapIndex;
+const mapLayersSelector = createSelector(
+  splitMapSelector,
+  splitMapIndexSelector,
+  getMapLayersFromSplitMaps
+);
 // const layerOrderSelector = state => state.visState.layerOrder;
 const layersToRenderSelector = createSelector(
   layersSelector,
   layerDataSelector,
   mapLayersSelector,
   // {[id]: true \ false}
-  (layers, layerData, mapLayers) =>
+  (layers, layerData, splitMapLayers) =>
     layers.reduce(
-      (accu: object, layer: any, idx: number) => ({
+      (accu: object, layer: KeplerLayer, idx: number) => ({
         ...accu,
-        [layer.id]: layer.shouldRenderLayer(layerData[idx]) && _isVisibleMapLayer(layer, mapLayers) // eslint-disable-line
+        [layer.id]:
+          layer.config.isVisible &&
+          layer.shouldRenderLayer(layerData[idx]) &&
+          _isVisibleSplitMapLayer(layer, splitMapLayers) &&
+          layer.overlayType === 'deckgl' // eslint-disable-line
       }),
       {}
     )
 );
-/* component private functions */
-function _isVisibleMapLayer(layer: any, mapLayers: any) {
-  // if layer.id is not in mapLayers, don't render it
-  return !mapLayers || (mapLayers && mapLayers[layer.id]);
+
+function _isVisibleSplitMapLayer(layer: KeplerLayer, splitMapLayers?: SplitMapLayers) {
+  console.log('layer', layer);
+  console.log('splitMapLayers', splitMapLayers);
+
+  // Undefined splitMapLayers means there isn't a split map, so don't refer to it for layer visibility.
+  // If splitMapLayers is defined, it means there is a split map and the upstream caller has selected the map to render in video.
+  return !splitMapLayers || (splitMapLayers && splitMapLayers[layer.id]);
 }
 
 function _onLayerSetDomain(idx: number, colorDomain: any) {
@@ -44,9 +69,10 @@ function _onLayerSetDomain(idx: number, colorDomain: any) {
 function renderLayer(
   overlays: any,
   idx: number,
-  map: any,
+  map: KeplerGlState,
   viewState: MapViewState,
-  beforeId?: string
+  beforeId?: string,
+  isVisible: boolean
 ) {
   const {
     visState: {datasets, layers, layerData, hoverInfo, clicked, interactionConfig, animationConfig},
@@ -62,7 +88,10 @@ function renderLayer(
     onSetLayerDomain: (val: any) => _onLayerSetDomain(idx, val)
   };
 
-  // Layer is Layer class
+  if (!isVisible) {
+    return overlays;
+  }
+
   const layerOverlay = layer
     .renderLayer({
       data,
@@ -78,27 +107,32 @@ function renderLayer(
       deckLayer.clone({
         pickable: false,
         // @ts-expect-error MapboxOverlay layers are extended to include beforeId
-        beforeId
+        beforeId,
+        visible: true
       })
     );
   return overlays.concat(layerOverlay || []);
 }
 
-export function createKeplerLayers(map: any, viewState: MapViewState, beforeId?: string) {
-  const layersToRender = layersToRenderSelector(map);
+export function createKeplerLayers(map: KeplerGlState, viewState: MapViewState, beforeId?: string) {
+  const layersToRender = layersToRenderSelector(map, 0);
+  console.log('layersToRender', layersToRender);
   // returns an arr of DeckGL layer objects
   const {layerOrder, layerData, layers} = map.visState;
+  console.log('layerData', layerData);
+  console.log('layerOrder', layerOrder);
+  console.log('layers', layers);
+
   if (layerData && layerData.length) {
-    return layerOrder
-      .slice()
-      .reverse()
-      .filter(
-        (_, idx: number) => layers[idx].overlayType === 'deckgl' && layersToRender[layers[idx].id]
-      )
+    const overlays = [...layerOrder]
+      .map(layerId => ({layerId, visible: layersToRender[layerId]}))
       .reduce(
-        (overlays: any, _, idx: number) => renderLayer(overlays, idx, map, viewState, beforeId),
+        (overlays: any, layerMeta, idx) =>
+          renderLayer(overlays, idx, map, viewState, beforeId, layerMeta.visible),
         []
-      ); // Slicing & reversing to create same layer order as Kepler
+      ); // Create same layer order as Kepler
+    console.log('overlays', overlays);
+    return overlays;
   }
   return [];
 }
